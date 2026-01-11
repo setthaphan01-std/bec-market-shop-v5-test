2bcba645282dc02fda848f3c661f548d7f4a49d1
setthaphan01-std <setthaphan01@gmail.com>
2026-01-11 19:01:22 +0700

    BEC

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -2,8 +2,8 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@^2.45.4';
 
 // ดึงค่าจาก process.env ที่กำหนดใน vite.config.ts
-const supabaseUrl = process.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.VITE_SUPABASE_URL || 'https://riuvrddhumzxfjsvpftd.supabase.co';
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpdXZyZGRodW16eGZqc3ZwZnRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxMjU4NDksImV4cCI6MjA4MzcwMTg0OX0.8pTatJ7u3hcDnqvuHdbqTSZSVijSkvkpqnHtBVOKuEI';
 
 // สร้าง Dummy Client เพื่อป้องกันแอปพัง (Crash) หากยังไม่มีการตั้งค่า Supabase
 const createDummyClient = () => {
--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   define: {
     'process.env': {
       API_KEY: JSON.stringify(process.env.API_KEY || 'PLACEHOLDER_API_KEY'),
-      VITE_SUPABASE_URL: JSON.stringify(process.env.VITE_SUPABASE_URL || 'https://psvjxaqzqumweqdvsqpz.supabase.co'),
-      VITE_SUPABASE_ANON_KEY: JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzdmp4YXF6cXVtd2VxZHZzcXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwODkxMTgsImV4cCI6MjA4MzY2NTExOH0._6MsYCtlGuThLeYPd-aRS29Cbtmvh0Lhu7wacV-po6Y')
+      VITE_SUPABASE_URL: JSON.stringify(process.env.VITE_SUPABASE_URL || 'https://riuvrddhumzxfjsvpftd.supabase.co'),
+      VITE_SUPABASE_ANON_KEY: JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY || 'Supabase not configuredeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpdXZyZGRodW16eGZqc3ZwZnRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxMjU4NDksImV4cCI6MjA4MzcwMTg0OX0.8pTatJ7u3hcDnqvuHdbqTSZSVijSkvkpqnHtBVOKuEI')
     }
   },
   server: {
